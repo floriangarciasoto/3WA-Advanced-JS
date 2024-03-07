b018b90e990858092ceeb077b146ed8189c32987
Your Name <floriangarciasoto@outlook.fr>
2024-03-07 10:59:35 +0100

    todo-list: back to 1 form

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,15 +1,40 @@
-import { getTodos } from "./lib/storage/localStorage";
-import { getFormHTML, addSubmitTodoFormsEventListeners } from "./lib/DOM/forms";
-import { addDisplayTodoFormsEventListeners } from "./lib/DOM/display";
+import { v4 as uuidv4 } from "uuid";
+import { getTodos, setTodos } from "./lib/storage/localStorage";
+import { getTodoHTML } from "./lib/DOM/display";
 
 const todos = getTodos();
 
+const todosList = document.querySelector('#todos-list');
+const todosSl = document.querySelector('#todos-sl');
+const form = document.querySelector('form');
+
 function init() {
 
-	document.querySelector('#todo-create form').innerHTML = getFormHTML();
+	for (id in todos) {
+		todosList.innerHTML += getTodoHTML(id,todos[id]);
+		todosSl.innerHTML += `<option value="${id}">`+todos[id]['name']+`</option>`;
+	}
 
-	addDisplayTodoFormsEventListeners();
-	addSubmitTodoFormsEventListeners();
+	todosSl.addEventListener('change',(e) => {
+		console.log(e.target.value);
+	});
+
+	form.addEventListener('submit',function(e){
+
+		e.preventDefault();
+
+		const formData = new FormData(form);
+		const todo = {};
+		for (var [key, value] of formData.entries()) todo[key] = value;
+		const {name, priority, description, duedate} = todo;
+
+		const todoID = uuidv4();
+		todosList.innerHTML += getTodoHTML(todoID,todo);
+
+		todos[todoID] = todo;
+		setTodos(todos);
+
+	});
 	
 }
 
--- todo-list/lib/DOM/display.js
+++ todo-list/lib/DOM/display.js
@@ -1,14 +1,12 @@
-export function addDisplayTodoFormsEventListeners() {
-
-	document.querySelectorAll('.show-edit.non-listened, .cancel-edit.non-listened').forEach(element => {
-		element.addEventListener('click',e => {
-			
-			e.preventDefault();
-			e.target.parentNode.parentNode.classList.toggle('in-edit');
-			
-		});
-		element.classList.remove('non-listened');
-	});
-
+export function getTodoHTML(todoID,todo) {
+	const {name, priority, description, duedate} = todo;
+	return `
+	<fieldset>
+		<legend>${todoID}</legend>
+		<h2>${name}</h2>
+		<p>Priorité : <span>`+['Pas urgent','Normal','Urgent'][priority]+`</span></p>
+		<p>${description}</p>
+		<p>A faire pour le <span>${duedate}</span></p>		
+	</fieldset>
+	`;
 }
-
